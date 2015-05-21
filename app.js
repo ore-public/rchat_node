6bb7a34ffb183b15bfe81f235a78659e50d94fb3
Masaya Konishi <konishi@office-ore.net>
2015-05-21 16:26:19 +0900

    ルームへの接続と、クライアントへのメッセージ配信処理を実装

--- app.js
+++ app.js
@@ -14,10 +14,16 @@ var resJson = function(res, obj){
 }
 
 io.sockets.on('connection', function(socket){
+
+  socket.on('connect_room', function(data){
+    socket.join(data.room_id);
+    console.log('connected room room_id: ' + data.room_id);
+  });
+
   app.post('/comment/:room_id', function(req, res) {
     room_id = req.params.room_id;
-    //res.send('comment no: ' + room_id);
-    console.log('room_id: ' + room_id);
+    socket.broadcast.to(room_id).emit('emit_comment', room_id);
+    console.log('comment delivery room_id: ' + room_id);
     resJson(res, {response: "OK"});
   });
 });
